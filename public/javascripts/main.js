8db55d56d2a7abbb215f8b439e883c17711f5c89
Jennifer Cheng <ashowga@gmail.com>
2017-06-16 15:19:17 -0700

    Add question list when page loads with ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,37 @@
 // Write chatr code here!
+
+```function getMessages () {
+  // In its basic form, fetch is a function that
+  // takes a URL as its first argument.
+  // It returns a promise that resolves with the response object.
+  // The body of the response can be further processed with the
+  // methods .text(), .html(), .json(), etc to parse it in
+  // its respective data format.
+
+  // If the full URL isn't provided, fetch will automatically
+  // prefix the URL with the hosting domain whichw ould be
+  // http://localhost:3434 in this case.
+  return fetch(/*http://localhost:3434*/'/messages')
+    .then(response => response.json());
+}
+
+function renderMessages (messages) {
+  const messagesUl = document.getElementById('messages');
+  const messagesHTML = messages.map(message => {
+    return `
+      <li>
+        <p>${message.content}</p>
+        <p>
+          <strong>${message.id}</strong>
+          <a data-id="${message.id}" href class="delete-button">Delete</a>
+        </p>
+      </li>
+    `;
+  }).join('');
+  messagesUl.innerHTML = messagesHTML;
+}
+getMessages().then(renderMessages);
+```
+
+Jump
+Mark as read (esc)
